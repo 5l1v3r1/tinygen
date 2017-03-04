40bfa5fc92225b7c01fd65b72c82584d9d2da8d8
Kevin Froman <kevinfroman12@gmail.com>
2017-03-04 11:30:41 -0600

    fixed html issue in rss

--- tgrss.py
+++ tgrss.py
@@ -1,8 +1,14 @@
 import configparser, sqlite3
+markdownSupport = True
+try:
+    import markdown
+except ImportError:
+    markdownSupport = False
 
 def updateRSS(config):
     conn = sqlite3.connect('.data/posts.db')
     c = conn.cursor()
+    descText = ''
     rss = '''<?xml version="1.0" encoding="UTF-8" ?>
     <rss version="2.0">
 
@@ -11,10 +17,14 @@ def updateRSS(config):
     <link>http://''' + config['SITE']['domain'] + '''</link>
     <description>''' + config['BLOG']['description'] + '''</description>'''
     for row in c.execute('SELECT * FROM Posts ORDER BY ID DESC'):
+        if not markdownSupport:
+            descText = open('source/posts/' + row[1] + '.html', 'r').read().splitlines()[0].replace('<', '&lt;').replace('>', '&gt;')
+        else:
+            descText = markdown.markdown(open('source/posts/' + row[1] + '.html', 'r').read().splitlines()[0]).replace('<', '&lt;').replace('>', '&gt;')
         rss = rss + '''<item>
-            <title>''' + row[1].replace('<', '&lt;').replace('>', '&gt;').replace('-', ' ') + '''</title>
-            <link>http://''' + config['SITE']['domain'] + '''blog/''' + row[1] + '''.html</link>
-            <description>''' + open('source/posts/' + row[1] + '.html', 'r').read().splitlines()[0].replace('<', '&lt;').replace('>', '&gt;') + '''</description></item>'''
+        <title>''' + row[1].replace('<', '&lt;').replace('>', '&gt;').replace('-', ' ') + '''</title>
+        <link>http://''' + config['SITE']['domain'] + '''blog/''' + row[1] + '''.html</link>
+        <description>''' + descText + '''</description></item>'''
     rss = rss + '</channel></rss>'
     open('generated/blog/feed.rss', 'w').write(rss)
     conn.close()
